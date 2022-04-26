2bf3ae47ea48ada4104110bf82976c20043b6392
Ion G <98644414+Johny36@users.noreply.github.com>
2022-04-26 19:43:10 +0300

    Add files via upload

---
+++ Cycle-While-BOO_YA.py
@@ -0,0 +1,13 @@
+
+n = "Yes"
+
+
+while True:
+    print("BOO YA!")
+    answer = input("Do you wan't me to stop ? \n ")
+    if answer == n:
+        print()
+        break
+    else:
+        continue
+
